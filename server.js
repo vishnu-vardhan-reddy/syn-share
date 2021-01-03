938a7ddfd89fbaa5b0270faf0f0e2f1af021b76c
Vishnuvardhan Reddy <vishnuvardhan4133@gmail.com>
2021-01-03 21:41:14 +0530

    fixed error

--- server.js
+++ server.js
@@ -15,10 +15,14 @@ app.use(
 );
 
 connectDB();
+app.get("/", (req, res) => {
+  res.send("hello");
+});
 
 //Template engine
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
+
 //routes
 app.use("/api/files", require("./routes/files"));
 app.use("/files", require("./routes/show"));
